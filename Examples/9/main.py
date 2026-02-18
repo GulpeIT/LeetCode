2b9d0110b90c2f5a8e0acb6c3cac68a449ca807a
GulpeIT <grishawork-01@mail.ru>
2026-02-18 14:23:48 +0300

    add unit test

--- Examples/9/main.py
+++ Examples/9/main.py
@@ -1,21 +1,22 @@
-
-# class Solution:
-#     def isPalindrome(x: int) -> bool:
-#         revers = list(str(x))
-#         revers.reverse()
-        
-#         if list(str(x)) == revers:
-#             return True
-#         else:
-#             return False
+import unittest
 
 class Solution:
-    def isPalindrome(x: int) -> bool:
+    def isPalindrome(self, x: int) -> bool:
+        revers = list(str(x))
+        revers.reverse()
         
-        if (x - x) == 0:
+        if list(str(x)) == revers:
             return True
         else:
             return False
 
+class UnitTest(unittest.TestCase):
+    def test_nextRomanToInt(self):
+        s = Solution()
+        
+        self.assertEqual(s.isPalindrome(121), True)
+        self.assertEqual(s.isPalindrome(-121), False)
+        self.assertEqual(s.isPalindrome(10), False)
+
 if __name__ == "__main__":
-    print(Solution.isPalindrome(x=1222))
+    unittest.main()
