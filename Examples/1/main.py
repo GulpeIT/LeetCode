952fb6a4e1210a60f009dfe3ec19d747ab924241
GulpeIT <138732780+GulpeIT@users.noreply.github.com>
2026-05-25 06:47:50 +0300

    add test, and remove old

--- Examples/1/main.py
+++ Examples/1/main.py
@@ -1,20 +1,17 @@
 from typing import List
 
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         pass
+
 class Solution:
     def twoSum(nums: List[int], target: int) -> List[int]:
-        count_i = 0
-        for i in nums:
-            count_j = 0
-            for j in nums:
-                if count_j != count_i:
-                    if (i + j) == target:
-                        result = []
-                        result.append(count_i)
-                        result.append(count_j)
-                count_j += 1
-            count_i += 1
+        hashmap = {}
+        for i in range(len(nums)):
+            num = target - nums[i]
+            if num in hashmap:
+                return [i, hashmap[num]]
+            hashmap[nums[i]] = i
 
 if __name__ == "__main__":
-    Solution.twoSum(nums=[2,7,11,15], target=9) # [0, 1]
-    Solution.twoSum(nums=[3, 2, 4], target=6) # [1, 2]
-    Solution.twoSum(nums=[3, 3], target=6) # [0, 1]
+    print(Solution.twoSum([2, 7, 11, 15], 9))
--- Examples/Yandex/main.py
+++ Examples/Yandex/main.py
@@ -1,18 +1,32 @@
+import unittest
+
 # Найти все простые числа меньше или равные заданному числу N.
 
 class Solution:
-    def countPrimes(self, n: int) -> int:
-        primes = []
-        is_prime = [True] * (n + 1)
-        for i in range(2, n + 1):
-            if is_prime[i]:
-                primes.append(i)
-            for p in primes:
-                if i * p > n:
-                    break
-                is_prime[i * p] = False
-                if i % p == 0:
-                    break
-        return primes.count
+    def findSimple(n: int) -> dict | None:
+        if n <= 1:
+            return None
+        
+        simples: dict = []
+        
+        for i in range(n):
+            
+            ...
+
+
+class TestSolution(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(Solution.findSimple(n = 1), None)
+    
+    def test_11(self):
+        self.assertEqual(Solution.findSimple(n = 11), [2, 3, 5, 7, 11])
+    
+    def test_112(self):
+        self.assertEqual(Solution.findSimple(n = 112), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109])
+    
+    
+
+if __name__ == "__main__":
+    unittest.main()
 
 # Найти наибольший общий делитель всех чисел в заданном диапазоне [L, R].
